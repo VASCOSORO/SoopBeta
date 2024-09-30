51253ded9a72b2a3bd4ca963af411452192a1524
VASCOSORO <virtualgraf00@gmail.com>
2024-09-30 18:43:06 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -546,7 +546,7 @@ def modulo_ventas():
             }
             credito_cliente = cliente_data.get('Estado Credito', 'Pagos regulares')  # Asumiendo que 'Estado Credito' existe
             color_credito = opciones_credito.get(credito_cliente, '🟡')  # Valor por defecto si no coincide
-            st.write(f"**Estado de crédito:** {color_credito} {credito_cliente}")
+            st.write(f"**Estattuss:** {color_credito} {credito_cliente}")
 
         with col3:
             forma_pago = st.selectbox(
